6ab3ba79f6e39b43c5dcc6fec6df2fab4e1f620d
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-03-17 10:54:11 +0100

    File updated at 17.03.2025, 10:54:12

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -1,34 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        result = []
-        def isValid(s: str) -> bool:
-            brack = {
-                "(":")",
-                "[":"]",
-                "{":"}"
-
-            }
-            open_brackets = []
-            for bracket in s:
-                if bracket in "({[":
-                    open_brackets.append(bracket)
-                else:
-                    if len(open_brackets) == 0 :return False
-                    if bracket == brack[open_brackets[-1]]:
-                        open_brackets.pop()
-                    else:
-                        return False
-            if  len(open_brackets) == 0:
-                return True
-            return False
-        def generate_rec(res,n):
-            if n == 0:
-                if isValid(res):
-                    result.append(res)
+        ans = []
+        
+        def dfs(openP, closeP, s):
+            if openP == closeP == n:
+                ans.append(s)
                 return
-            else:
-                generate_rec(res+")",n-1)
-                generate_rec(res+"(",n-1)
-        generate_rec("",n*2)
-        return result
+
+            if openP < n:
+                dfs(openP + 1, closeP, s + "(")
             
+            if closeP < openP:
+                dfs(openP, closeP + 1, s + ")")
+        
+        dfs(1, 0, "(")
+        return ans
