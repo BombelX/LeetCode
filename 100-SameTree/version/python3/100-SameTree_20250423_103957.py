634e0b65b467efa0d2d19505b0f4d1caf995a6ff
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-04-23 10:39:56 +0200

    Create file

---
+++ 100-SameTree/version/python3/100-SameTree_20250423_103957.py
@@ -0,0 +1,33 @@
+# Last updated: 23.04.2025, 10:39:57
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+
+        if root is None:
+            return result
+        
+        queue = []
+        queue.append(root)
+
+        while queue:
+            level = []
+            size = len(queue)
+
+            for i in range(0, size):
+                node = queue.pop(0)
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.insert(0, level)
+
+        return result
