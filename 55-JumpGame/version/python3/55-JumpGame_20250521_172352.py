b09b0b1a485979647c473a8306aa6f50a2420e3c
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-05-21 17:23:53 +0200

    Create file

---
+++ 55-JumpGame/version/python3/55-JumpGame_20250521_172352.py
@@ -0,0 +1,36 @@
+# Last updated: 21.05.2025, 17:23:52
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+        if n == 1:
+            return True
+        result = False
+        possible = [-1]*n
+        def jump(pos):
+            nonlocal n,result,possible
+            if result:
+                return False
+            if pos == n-1:
+                result == True
+                return True
+            if pos >n-1:
+                return False
+            if possible[pos] == 0:
+                return False
+            else:
+                flag = False
+                for i in reversed(range(1,nums[pos]+1)):
+                    res = jump(pos+i)
+                    if res:
+                        flag = True
+                        return True
+                possible[pos] = 0
+
+
+        for i in reversed(range(1,nums[0]+1)):
+            x = jump(i)
+            if x: 
+                return True
+
+        return result
