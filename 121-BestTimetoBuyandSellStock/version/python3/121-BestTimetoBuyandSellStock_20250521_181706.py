8b1da625494a1b16f56029fd2869649866ca0a60
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-05-21 18:17:06 +0200

    Create file

---
+++ 121-BestTimetoBuyandSellStock/version/python3/121-BestTimetoBuyandSellStock_20250521_181706.py
@@ -0,0 +1,9 @@
+# Last updated: 21.05.2025, 18:17:06
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        best_buy = prices[0]
+        best_profit = 0
+        for price in prices[1:]:
+            best_buy = min(price,best_buy)
+            best_profit = max(best_profit,price-best_buy)
+        return best_profit
