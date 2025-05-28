08938948a8e0139590c701dd987fc2c17b29aaa3
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-05-28 19:18:37 +0200

    Create 64-MinimumPathSum

---
+++ 64-MinimumPathSum/version/python/64-MinimumPathSum_20250528_191839.py
@@ -0,0 +1,16 @@
+# Last updated: 28.05.2025, 19:18:39
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n,m = len(grid),len(grid[0])
+        for lvl in range(n):
+            for box in range(m):
+                if (lvl-1>=0) and (box-1>=0):
+                    grid[lvl][box] +=min(grid[lvl-1][box],grid[lvl][box-1])
+                elif box-1>=0:
+                    grid[lvl][box] += grid[lvl][box-1]
+                elif lvl-1>=0:
+                    grid[lvl][box] += grid[lvl-1][box]
+                else:
+                    pass
+        return grid[-1][-1]
+
