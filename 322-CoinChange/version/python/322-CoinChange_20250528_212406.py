da5c308e308e41910ac9b88c3517801287e6ba1b
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-05-28 21:24:03 +0200

    Create 322-CoinChange

---
+++ 322-CoinChange/version/python/322-CoinChange_20250528_212406.py
@@ -0,0 +1,27 @@
+# Last updated: 28.05.2025, 21:24:06
+# idk why this work xd
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = {}
+        def coin_v(amount,coins):
+            if amount == 0:
+                return 1
+            elif amount<0:
+                return float('inf')
+            nonlocal dp
+            min_coins = float('inf')
+            for coin in coins:
+                if coin>amount:
+                    break
+                if amount-coin in dp:
+                    min_coins = min(min_coins,dp[amount-coin])
+                else:
+                    x = coin_v(amount-coin,coins)
+                    min_coins = min(min_coins,x)
+            dp[amount] = min_coins+1
+            return min_coins+1
+        x = coin_v(amount,sorted(coins))
+        if x == float('inf'):
+            return -1
+        else:
+            return x-1
