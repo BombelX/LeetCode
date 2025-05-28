94155e3398568914f0d4572bbd04730981187c30
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-05-28 19:20:59 +0200

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.py
@@ -0,0 +1,38 @@
+# Last updated: 28.05.2025, 19:21:02
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        t1,t2 = nums1,nums2
+        n,m = len(t1),len(t2)
+        merged_tab = [0]*(m+n)
+        x = 0
+        j = 0
+        for ind in range(m+n):
+            if x<n and j<m:
+                if t1[x]<t2[j]:
+                    merged_tab[ind] = t1[x] 
+                    x+=1
+                else:
+                    merged_tab[ind] = t2[j]
+                    j+=1
+            else:
+                if x<n:
+                    merged_tab[ind] = t1[x]
+                    x+=1
+                else:
+                    merged_tab[ind] = t2[j]
+                    j+=1
+        # print(merged_tab)
+        if (n+m)%2 == 0:
+            return (merged_tab[int((n+m)//2)-1] + merged_tab[int((n+m)//2)])/2
+        else :
+            return merged_tab[int((n+m)/2)]
+        
+                    
+
+        
+        
+        
+                    
+
+        
+        
