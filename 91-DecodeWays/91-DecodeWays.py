288169754e1b772a1c9f7c9c86c2875f9b8474cc
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-05-28 19:20:36 +0200

    Create 91-DecodeWays

---
+++ 91-DecodeWays/91-DecodeWays.py
@@ -0,0 +1,25 @@
+# Last updated: 28.05.2025, 19:20:38
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        history = {}
+
+        def decode(s, sub):
+            if s == '':
+                return 1
+            elif s[0] == '0':
+                return 0
+
+            if (s, sub) in history:
+                return history[(s, sub)]
+            summ = 0
+            summ += decode(s[1:], 0)
+            if len(s) >= 2:
+                two = int(s[:2])
+                if 10 <= two <= 26:
+                    summ += decode(s[2:], two)
+            history[(s, sub)] = summ
+            return summ
+
+        return decode(s, 0)
