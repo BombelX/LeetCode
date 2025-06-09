02acb994002109bf926fed664a04cb8da5c07f98
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-06-09 15:06:18 +0200

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/version/python/1025-DivisorGame_20250609_150628.py
@@ -0,0 +1,7 @@
+# Last updated: 9.06.2025, 15:06:28
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        if n%2 == 0:
+            return True
+        else:
+            return False
