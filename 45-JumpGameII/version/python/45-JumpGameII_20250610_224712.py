de99c1e4ef412923a11b478362f57f3b107a2e9c
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-06-10 22:47:12 +0200

    Create 45-JumpGameII

---
+++ 45-JumpGameII/version/python/45-JumpGameII_20250610_224712.py
@@ -0,0 +1,18 @@
+# Last updated: 10.06.2025, 22:47:12
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp = {}
+        def jmp(i):
+            if i in dp:
+                return dp[i]
+            if i >= len(nums)-1:
+                return 0
+
+            dp_val = float('inf')
+
+            for x in range(i+1,i+nums[i]+1):
+                dp_val = min(dp_val,jmp(x)) 
+            
+            dp.update({i:dp_val+1})
+            return dp_val+1
+        return jmp(0)
