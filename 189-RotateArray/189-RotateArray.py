120959a2012142bb77a9d83ccf4edc7870dcd5bd
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-11-28 19:22:58 +0100

    File updated at 28.11.2025, 19:22:58

--- 189-RotateArray/189-RotateArray.py
+++ 189-RotateArray/189-RotateArray.py
@@ -1,21 +1,23 @@
-# Last updated: 28.11.2025, 19:20:20
+# Last updated: 28.11.2025, 19:22:58
 1class Solution:
 2    def rotate(self, nums: List[int], k: int) -> None:
 3        n = len(nums)
 4        cntr = 0 
-5        for i in range(k):
-6            index = i+k
-7            last = nums[i]
-8            while cntr < n and index%n !=i:
-9                cntr += 1
-10                tmp = nums[index%n]
-11                nums[index%n] = last
-12                last = tmp
-13                index += k
-14            nums[index%n] = last
-15            cntr += 1
-16
-17            if cntr == n:
-18                return nums
-19            
-20             
+5        k = k%n
+6        if k == 0 :return
+7        for i in range(k):
+8            index = i+k
+9            last = nums[i]
+10            while cntr < n and index%n !=i:
+11                cntr += 1
+12                tmp = nums[index%n]
+13                nums[index%n] = last
+14                last = tmp
+15                index += k
+16            nums[index%n] = last
+17            cntr += 1
+18
+19            if cntr == n:
+20                return nums
+21            
+22             
