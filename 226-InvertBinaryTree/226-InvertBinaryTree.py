4938b50234f8fd2215c76fdb1a23783294e4f9c2
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-11-28 22:07:01 +0100

    File updated at 28.11.2025, 22:07:02

--- 226-InvertBinaryTree/226-InvertBinaryTree.py
+++ 226-InvertBinaryTree/226-InvertBinaryTree.py
@@ -1,18 +1,27 @@
-# Last updated: 28.11.2025, 21:31:27
-1# Definition for a binary tree node.
-2# class TreeNode:
-3#     def __init__(self, val=0, left=None, right=None):
-4#         self.val = val
-5#         self.left = left
-6#         self.right = right
-7class Solution:
-8    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-9        def go(node):
-10            if node is None:
-11                return
-12            node.left,node.right = node.right,node.left
-13            go(node.left)
-14            go(node.right)
-15        go(root)
-16        return root
-17        
+# Last updated: 28.11.2025, 22:07:02
+1from collections import deque
+2
+3class Solution:
+4    def isValid(self, s: str) -> bool:
+5        stack = deque()
+6
+7        for bracket in s:
+8            if bracket == '(' or bracket == '[' or bracket == '{':
+9                stack.append(bracket)
+10            else:
+11                if not stack:
+12                    return False
+13                b = stack.pop()
+14                if b == '(' and bracket == ')':
+15                    continue
+16                elif b == '{' and bracket == '}':
+17                    continue
+18                elif b == '[' and bracket == ']':
+19                    continue
+20                else:
+21                    return False
+22        if stack:
+23            return False
+24        return True
+25
+26
