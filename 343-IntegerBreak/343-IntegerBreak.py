888f2732d7e5152b6fb3c5e5e84547b88dc1e98c
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-11-28 19:20:56 +0100

    Create 343-IntegerBreak

---
+++ 343-IntegerBreak/343-IntegerBreak.py
@@ -0,0 +1,18 @@
+# Last updated: 28.11.2025, 19:20:56
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = {}
+        def break_num(num):
+            if num <= 1:
+                return 1
+            mid = num//2+num%2
+            max_b = 1
+            for i in range(1,mid+1):
+                if num-i in dp:
+                    x = max(dp[num-i],num-i)
+                else:
+                    x = max(break_num(num-i),num-i)
+                max_b = max(max_b,x*i)
+            dp[num] = max_b
+            return max_b
+        return break_num(n)
