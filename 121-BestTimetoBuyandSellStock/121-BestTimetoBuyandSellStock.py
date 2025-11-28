9571f13e47c4145b79a7e4bfa63af6338b951509
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-11-28 17:38:59 +0100

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -0,0 +1,11 @@
+# Last updated: 28.11.2025, 17:38:59
+1class Solution:
+2    def maxProfit(self, prices: List[int]) -> int:
+3        min_meeted = float('inf')
+4        max_delta = float('-inf')
+5        for price in prices:
+6            max_delta = max(max_delta,price-min_meeted)
+7            min_meeted = min(min_meeted,price)
+8        if max_delta>0:
+9            return max_delta
+10        return 0
