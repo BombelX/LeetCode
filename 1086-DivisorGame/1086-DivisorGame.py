055af618a72b69799f007bec55da409ddba56fa6
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-11-28 19:20:46 +0100

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.py
@@ -0,0 +1,7 @@
+# Last updated: 28.11.2025, 19:20:46
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        if n%2 == 0:
+            return True
+        else:
+            return False
