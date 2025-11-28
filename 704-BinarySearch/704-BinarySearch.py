7bc711713f07e9d5789e8e5ffa5d1202fc133fd0
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-11-28 22:25:07 +0100

    File updated at 28.11.2025, 22:25:08

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,21 +1,16 @@
-# Last updated: 28.11.2025, 22:24:49
+# Last updated: 28.11.2025, 22:25:08
 1class Solution:
 2    def search(self, nums: List[int], target: int) -> int:
 3        left = 0
-4        right = len(nums) - 1  # Wskazujemy na ostatni element
+4        right = len(nums) - 1  
 5        
-6        # Używamy <=, żeby obsłużyć przypadek, gdy zostanie 1 element (left == right)
-7        while left <= right:
-8            mid = (left + right) // 2
-9            
-10            if nums[mid] == target:
-11                return mid
-12            
-13            elif nums[mid] < target:
-14                # Szukana liczba jest po prawej, więc start musi przeskoczyć mid
-15                left = mid + 1
-16            else:
-17                # Szukana liczba jest po lewej, więc koniec musi przeskoczyć mid
-18                right = mid - 1
-19                
-20        return -1
+6        while left <= right:
+7            mid = (left + right) // 2
+8            if nums[mid] == target:
+9                return mid
+10            elif nums[mid] < target:
+11                left = mid + 1
+12            else:
+13                right = mid - 1
+14                
+15        return -1
