076d3ea55e326dc6c2bd53548d61b2b05c00b37a
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-11-28 19:20:45 +0100

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.py
@@ -0,0 +1,19 @@
+# Last updated: 28.11.2025, 19:20:45
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 1
+        last_3 = 0
+        last_2  = 1
+        last_1 = 1
+
+        for i in range(n-3):
+            tmp = last_1+last_2+last_3
+            last_3 = last_2
+            last_2 = last_1
+            last_1 = tmp
+        return last_1+last_2+last_3
