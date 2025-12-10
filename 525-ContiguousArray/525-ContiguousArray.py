764e61ade704f7db4c4f5091f061f846eb7a7a64
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2025-12-10 20:23:11 +0100

    Create 525-ContiguousArray

---
+++ 525-ContiguousArray/525-ContiguousArray.py
@@ -0,0 +1,19 @@
+# Last updated: 10.12.2025, 20:23:22
+1class Solution:
+2    def findMaxLength(self, nums: List[int]) -> int:
+3        first_visitation = {0:-1}
+4        max_len = 0
+5        count = 0
+6        for i,num in enumerate(nums):
+7
+8            if num == 1:
+9                count +=1
+10            else:
+11                count -=1
+12
+13            if count in first_visitation:
+14                curr_len = i-first_visitation[count]
+15                max_len = max(max_len,curr_len)
+16            else:
+17                first_visitation[count] = i
+18        return max_len
