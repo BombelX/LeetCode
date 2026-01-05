10051a2d784633e5404d090bf6d8e5b9b0a1787c
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-01-05 14:08:56 +0100

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.py
@@ -0,0 +1,22 @@
+# Last updated: 5.01.2026, 14:08:57
+1class Solution:
+2    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+3        solution = set()
+4        
+5
+6        def recu(number,tab):
+7            if number == target:
+8                solution.add(tuple(sorted(tab[:])))
+9                return
+10            elif number> target:
+11                return 
+12            else:
+13                for candidate in candidates:
+14                    tab.append(candidate)
+15                    recu(number+candidate,tab)
+16                    tab.pop()
+17            
+18        recu(0,[])
+19        res = [list(x) for x in solution]
+20        
+21        return res
