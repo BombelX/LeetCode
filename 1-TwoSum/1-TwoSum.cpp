e9ec73fd38aeaba632ca0659d19d3974fe4746c8
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-02-22 20:14:45 +0100

    File updated at 22.02.2026, 20:14:46

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,23 +1,25 @@
-// Last updated: 22.02.2026, 20:13:20
+// Last updated: 22.02.2026, 20:14:46
 1#include <unordered_map>
 2using namespace std;
 3class Solution {
 4public:
 5    vector<int> twoSum(vector<int>& nums, int target) {
-6        unordered_map<int,int> visited;
-7        vector<int> tab(2);
-8        
-9        int n = nums.size();
-10        cout << n << endl;
-11        for (int i=0 ;i<n; ++i){
-12            int missing = target - nums[i];
-13            if (visited.count(missing) > 0){
-14                tab[0] = visited[missing];
-15                tab[1] = i;
-16                break;
-17            }
-18            visited[nums[i]] = i;
-19        }
-20        return tab;
-21    }
-22};
+6        ios_base::sync_with_stdio(false);
+7        
+8        unordered_map<int,int> visited;
+9        vector<int> tab(2);
+10        
+11        int n = nums.size();
+12        cout << n << endl;
+13        for (int i=0 ;i<n; ++i){
+14            int missing = target - nums[i];
+15            if (visited.count(missing) > 0){
+16                tab[0] = visited[missing];
+17                tab[1] = i;
+18                break;
+19            }
+20            visited[nums[i]] = i;
+21        }
+22        return tab;
+23    }
+24};
