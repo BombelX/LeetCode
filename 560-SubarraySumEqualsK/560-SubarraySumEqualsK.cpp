25a9f70a46521b88520f3666b62b7c03357bafcc
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-02-22 21:23:42 +0100

    File updated at 22.02.2026, 21:23:43

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -1,38 +1,39 @@
-// Last updated: 22.02.2026, 21:23:21
+// Last updated: 22.02.2026, 21:23:43
 1#include <unordered_map>
 2
 3class Solution {
 4public:
 5    int subarraySum(vector<int>& nums, int k) {
 6        ios_base::sync_with_stdio(false);
-7        if (nums.size() == 1 ){
-8            if (nums[0] == k){
-9                return 1;
-10            }
-11            else{
-12                return 0;
-13            }
-14        }
-15
-16        unordered_map<int,int> visited;
-17        visited[0] = 1;
-18        visited[nums[0]] += 1;
-19        int res = 0;
-20
-21        if (nums[0] == k){
-22            res +=1;
-23        }
-24
-25        for (int i = 1;i < nums.size(); ++i){
-26            nums[i] += nums[i-1];
-27            int missing  = nums[i]-k;
-28            // cout << missing;
-29            if (visited.count(missing)>0 ){
-30                res += visited[missing];
-31            }
-32            visited[nums[i]] += 1;
-33        }
-34        return res;
-35
-36    }
-37};
+7        cin.tie(NULL);
+8        if (nums.size() == 1 ){
+9            if (nums[0] == k){
+10                return 1;
+11            }
+12            else{
+13                return 0;
+14            }
+15        }
+16
+17        unordered_map<int,int> visited;
+18        visited[0] = 1;
+19        visited[nums[0]] += 1;
+20        int res = 0;
+21
+22        if (nums[0] == k){
+23            res +=1;
+24        }
+25
+26        for (int i = 1;i < nums.size(); ++i){
+27            nums[i] += nums[i-1];
+28            int missing  = nums[i]-k;
+29            // cout << missing;
+30            if (visited.count(missing)>0 ){
+31                res += visited[missing];
+32            }
+33            visited[nums[i]] += 1;
+34        }
+35        return res;
+36
+37    }
+38};
