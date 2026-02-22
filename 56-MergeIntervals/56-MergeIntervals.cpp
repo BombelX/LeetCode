21b8c2b484fffb25fe432ecd40f0179c1cd18607
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-02-22 22:34:05 +0100

    File updated at 22.02.2026, 22:34:06

--- 56-MergeIntervals/56-MergeIntervals.cpp
+++ 56-MergeIntervals/56-MergeIntervals.cpp
@@ -1,20 +1,22 @@
-// Last updated: 22.02.2026, 22:33:35
+// Last updated: 22.02.2026, 22:34:06
 1class Solution {
 2public:
 3
 4    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-5        sort(intervals.begin(), intervals.end());
-6        vector<vector<int>> new_intervals;
-7        new_intervals.push_back(intervals[0]);
-8        int new_interval = 0;
-9        for (int i = 1; i < intervals.size(); ++i) {
-10            if (new_intervals.back()[1] >= intervals[i][0]) {
-11                new_intervals.back()[1] = max(new_intervals.back()[1], intervals[i][1]);
-12            }
-13            else{
-14                new_intervals.push_back(intervals[i]);
-15            }
-16        }
-17        return new_intervals;
-18    }
-19};
+5        ios_base::sync_with_stdio(false);
+6        cin.tie(NULL);
+7        sort(intervals.begin(), intervals.end());
+8        vector<vector<int>> new_intervals;
+9        new_intervals.push_back(intervals[0]);
+10        int new_interval = 0;
+11        for (int i = 1; i < intervals.size(); ++i) {
+12            if (new_intervals.back()[1] >= intervals[i][0]) {
+13                new_intervals.back()[1] = max(new_intervals.back()[1], intervals[i][1]);
+14            }
+15            else{
+16                new_intervals.push_back(intervals[i]);
+17            }
+18        }
+19        return new_intervals;
+20    }
+21};
