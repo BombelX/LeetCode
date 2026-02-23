aebf91eb80962a8ac65caee89283f7c3c57e10b7
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-02-23 12:15:58 +0100

    File updated at 23.02.2026, 12:16:00

--- 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.cpp
+++ 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.cpp
@@ -1,37 +1,39 @@
-// Last updated: 23.02.2026, 12:15:06
+// Last updated: 23.02.2026, 12:16:00
 1#include <unordered_map>
 2
 3class Solution {
 4public:
 5    bool hasAllCodes(string s, int k) {
-6        unordered_map<int,bool> map;
-7        if (s.size() < k) return false;
-8        int tmp = 0;
-9        for (int i = 0; i < k ; i++){
-10            if(s[i] == '1'){
-11                tmp += pow(2,k-i-1);
-12            }
-13        }
-14        map[tmp] = true;
-15        // cout<<tmp<<endl;
-16
-17        for (int i=k; i<s.size(); i++){
-18            if(s[i-k] == '1'){
-19                tmp -= pow(2,k-1);
-20            }
-21            tmp = tmp * 2;
-22            if(s[i] == '1'){
-23                tmp += 1;
-24            }
-25            map[tmp] = true;
-26            // cout<<tmp<<endl;
-27            };
-28        int limit = pow(2,k)-1;
-29        for (int i=0; i<=limit;i++){
-30            if (map.count(i) == 0){
-31                return false;
-32            }
-33        }
-34        return true;
-35    }
-36};
+6        ios_base::sync_with_stdio(false);
+7        cin.tie(NULL);
+8        unordered_map<int,bool> map;
+9        if (s.size() < k) return false;
+10        int tmp = 0;
+11        for (int i = 0; i < k ; i++){
+12            if(s[i] == '1'){
+13                tmp += pow(2,k-i-1);
+14            }
+15        }
+16        map[tmp] = true;
+17        // cout<<tmp<<endl;
+18
+19        for (int i=k; i<s.size(); i++){
+20            if(s[i-k] == '1'){
+21                tmp -= pow(2,k-1);
+22            }
+23            tmp = tmp * 2;
+24            if(s[i] == '1'){
+25                tmp += 1;
+26            }
+27            map[tmp] = true;
+28            // cout<<tmp<<endl;
+29            };
+30        int limit = pow(2,k)-1;
+31        for (int i=0; i<=limit;i++){
+32            if (map.count(i) == 0){
+33                return false;
+34            }
+35        }
+36        return true;
+37    }
+38};
