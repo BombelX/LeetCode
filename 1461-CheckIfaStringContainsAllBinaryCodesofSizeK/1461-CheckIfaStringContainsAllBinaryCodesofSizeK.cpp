7aaecb0aea322ffe8348a9801c411e5f2a1332da
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-02-23 12:19:06 +0100

    File updated at 23.02.2026, 12:19:08

--- 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.cpp
+++ 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.cpp
@@ -1,4 +1,4 @@
-// Last updated: 23.02.2026, 12:16:00
+// Last updated: 23.02.2026, 12:19:08
 1#include <unordered_map>
 2
 3class Solution {
@@ -11,7 +11,7 @@
 10        int tmp = 0;
 11        for (int i = 0; i < k ; i++){
 12            if(s[i] == '1'){
-13                tmp += pow(2,k-i-1);
+13                tmp += 1 << (k-i-1);
 14            }
 15        }
 16        map[tmp] = true;
@@ -19,16 +19,16 @@
 18
 19        for (int i=k; i<s.size(); i++){
 20            if(s[i-k] == '1'){
-21                tmp -= pow(2,k-1);
+21                tmp -= 1 << (k-1);
 22            }
-23            tmp = tmp * 2;
+23            tmp = tmp *2;
 24            if(s[i] == '1'){
 25                tmp += 1;
 26            }
 27            map[tmp] = true;
 28            // cout<<tmp<<endl;
 29            };
-30        int limit = pow(2,k)-1;
+30        int limit = (1 << k)-1;
 31        for (int i=0; i<=limit;i++){
 32            if (map.count(i) == 0){
 33                return false;
