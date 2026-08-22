a6d6be620f9aa799f792b38b378c71ab123dc44b
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-08-22 22:52:14 +0200

    File updated at 22.08.2026, 22:52:14

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
@@ -1,14 +1,12 @@
-# Last updated: 22.08.2026, 22:52:05
+# Last updated: 22.08.2026, 22:52:14
 1class Solution:
 2    def searchInsert(self, nums: List[int], target: int) -> int:
 3        start,end  = 0, len(nums)
-4
-5        while start<end:
-6            mid = (start+end) // 2
-7            if nums[mid] < target:
-8                start = mid+1
-9            else:
-10                end = mid
-11        print(start)
-12        return start
-13    
+4        while start<end:
+5            mid = (start+end) // 2
+6            if nums[mid] < target:
+7                start = mid+1
+8            else:
+9                end = mid
+10        return start
+11    
