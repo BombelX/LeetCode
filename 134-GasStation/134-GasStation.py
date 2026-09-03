83dd0192d36eecc2832b68d3cdb3143e3565a63b
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-09-03 21:09:50 +0200

    File updated at 3.09.2026, 21:09:50

--- 134-GasStation/134-GasStation.py
+++ 134-GasStation/134-GasStation.py
@@ -1,4 +1,4 @@
-# Last updated: 3.09.2026, 21:09:42
+# Last updated: 3.09.2026, 21:09:50
 1class Solution:
 2    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
 3
@@ -7,40 +7,39 @@
 6        fuel_balance = 0
 7        for i in range(len(gas)):
 8            fuel_balance += gas[i] - cost[i]
-9            print(i,fuel_balance,start)
-10            if fuel_balance<0:
-11                start = i+1
-12                fuel_balance = 0
-13            
-14
-15        if  (sum(gas)-sum(cost)) < 0:
-16            return -1
-17        else:
-18            return start
-19        # dp = set()
-20        # def go(place,fuel,start):
-21        #     if (place,fuel) in dp:
-22        #         return False
-23        #     if fuel<0:
-24        #         dp.add((place,fuel))
-25        #         return False
-26        #     if place == start:
-27        #         return True
-28
-29        #     new_fuel = fuel + gas[place] - cost[place]
-30        #     if new_fuel<0:
-31        #         dp.add((place,fuel))
-32        #         return False
-33        #     res = go((place+1)%len(gas),new_fuel,start)
-34        #     if res:
-35        #         return True
-36        #     else:
-37        #         dp.add((place,fuel))
-38        #         return False
-39        # for start,fuel in enumerate(gas):
-40        #     if go((start+1)%len(gas),fuel-cost[start],start):
-41        #         return start
-42
-43        return -1
+9            if fuel_balance<0:
+10                start = i+1
+11                fuel_balance = 0
+12            
+13
+14        if  (sum(gas)-sum(cost)) < 0:
+15            return -1
+16        else:
+17            return start
+18        # dp = set()
+19        # def go(place,fuel,start):
+20        #     if (place,fuel) in dp:
+21        #         return False
+22        #     if fuel<0:
+23        #         dp.add((place,fuel))
+24        #         return False
+25        #     if place == start:
+26        #         return True
+27
+28        #     new_fuel = fuel + gas[place] - cost[place]
+29        #     if new_fuel<0:
+30        #         dp.add((place,fuel))
+31        #         return False
+32        #     res = go((place+1)%len(gas),new_fuel,start)
+33        #     if res:
+34        #         return True
+35        #     else:
+36        #         dp.add((place,fuel))
+37        #         return False
+38        # for start,fuel in enumerate(gas):
+39        #     if go((start+1)%len(gas),fuel-cost[start],start):
+40        #         return start
+41
+42        return -1
+43
 44
-45
