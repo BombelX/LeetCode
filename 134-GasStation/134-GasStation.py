f50d4e48ca414e333a217b820e4ff48491236e92
Adam Rassem <103076121+BombelX@users.noreply.github.com>
2026-09-03 21:33:12 +0200

    File updated at 3.09.2026, 21:33:12

--- 134-GasStation/134-GasStation.py
+++ 134-GasStation/134-GasStation.py
@@ -1,45 +1,17 @@
-# Last updated: 3.09.2026, 21:11:34
+# Last updated: 3.09.2026, 21:33:12
 1class Solution:
-2    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-3
-4        sum_fuel = 0
-5        start = 0
-6        fuel_balance = 0
-7        for i in range(len(gas)):
-8            sum_fuel  += gas[i]-cost[i]
-9            fuel_balance += gas[i] - cost[i]
-10            if fuel_balance<0:
-11                start = i+1
-12                fuel_balance = 0
-13            
-14        if  sum_fuel < 0:
-15            return -1
-16        else:
-17            return start
-18        # dp = set()
-19        # def go(place,fuel,start):
-20        #     if (place,fuel) in dp:
-21        #         return False
-22        #     if fuel<0:
-23        #         dp.add((place,fuel))
-24        #         return False
-25        #     if place == start:
-26        #         return True
-27
-28        #     new_fuel = fuel + gas[place] - cost[place]
-29        #     if new_fuel<0:
-30        #         dp.add((place,fuel))
-31        #         return False
-32        #     res = go((place+1)%len(gas),new_fuel,start)
-33        #     if res:
-34        #         return True
-35        #     else:
-36        #         dp.add((place,fuel))
-37        #         return False
-38        # for start,fuel in enumerate(gas):
-39        #     if go((start+1)%len(gas),fuel-cost[start],start):
-40        #         return start
-41
-42        return -1
-43
-44
+2    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+3        letters_cnt = 26*[0]
+4
+5        for letter in magazine:
+6            letters_cnt[ord(letter)%26] += 1
+7        
+8        for letter in ransomNote:
+9            letters_cnt[ord(letter)%26] -= 1
+10        
+11        for c in letters_cnt:
+12            if c < 0:
+13                return False
+14
+15        return True
+16
